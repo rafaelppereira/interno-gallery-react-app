73b2e488ec2e8c87e89cbf3877c1b8e126cecfa3
Rafael <rafaelsantospereira03@gmail.com>
2021-11-10 02:17:21 -0300

    fisrt commit

---
+++ src/App.styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #27282f;
+  color: white;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 30px 0;
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+export const ScreenWarning = styled.div`
+  text-align: center;
+
+  .emoji {
+    font-size: 50px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const PhotoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+
+  @media (max-width:900px) {
+    grid-template-columns: 1fr;
+    padding: 30px;
+  }
+`;
+
+export const UploadForm = styled.form`
+  background-color: #3d3f43;
+  padding: 15px;
+  border-radius: 10px;
+  margin-bottom: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0 20px 30px 20px;
+
+  input[type=submit] {
+    background-color: #756df4;
+    border: 0;
+    color: white;
+    padding: 8px 16px;
+    font-size: 15px;
+    border-radius:10px;
+    margin: 0 20px;
+    cursor: pointer;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, FormEvent } from 'react';
+import * as C from './App.styles';
+import * as Photos from './services/photos';
 
-function App() {
+import { PhotoItem } from './components/PhotoItem';
+
+import { Photo } from './types/photo';
+
+import { Toaster, toast } from 'react-hot-toast';
+
+const App = () => {
+
+  const [uploading, setUploading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, [])
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if (file && file.size > 0) {
+      setUploading(true);
+      let result = await Photos.insert(file);
+      setUploading(false);
+
+      if (result instanceof Error) {
+        // alert(`${result.name} - ${result.message}`);
+        toast.error("Arquivo não permitido")
+      } else {
+        // Adiciona na lista
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+        toast.success('Foto Adicionada com sucesso!')
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image" />
+          <input type="submit" value="Enviar" />
+          {uploading && "Enviando..."}
+        </C.UploadForm>
+
+        {loading && 
+          <C.ScreenWarning>
+            <div className="emoji">🤚</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 && 
+          <C.PhotoList>
+            {photos.map((item, index)=>(
+              <PhotoItem key={index} url={item.url} name={item.name} />
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loading && photos.length === 0 && 
+          <C.ScreenWarning>
+            <div className="emoji">😔</div>
+            <div>Não há fotos cadastradas.</div>
+          </C.ScreenWarning>
+        }
+      </C.Area>
+    </C.Container>
+    <div>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+      />
     </div>
+    </>
   );
 }
 
